045941a8cb2174ca4ae96b853d76ecb091b43cbf
iyappan63moons-star <iyappan63moons@gmail.com>
2026-01-18 15:12:12 +0530

    lock method added.

--- src/node.rs
+++ src/node.rs
@@ -1,10 +1,12 @@
-use etcd_client::{Client, Compare, CompareOp, Txn, TxnOp, PutOptions};
+use etcd_client::{Client, Compare, CompareOp, Txn, TxnOp, PutOptions, LockOptions};
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::fs::OpenOptions;
 use std::time::Duration;
 
-pub async fn run_node(etcd_url: &str, node_addr: &str, node_name_ref: &str) {
+// Transaction Method
+
+pub async fn run_node_transactiom_method(etcd_url: &str, node_addr: &str, node_name_ref: &str) {
 
     let mut client = Client::connect([etcd_url], None).await.unwrap();
     let node_name = node_name_ref.to_string();
@@ -60,5 +62,56 @@ pub async fn run_node(etcd_url: &str, node_addr: &str, node_name_ref: &str) {
             // Election Failed. Leader Exists.
             tokio::time::sleep(Duration::from_secs(2)).await;
         }
+    }
+}
+
+
+// Lock Method
+
+pub async fn run_node(etcd_url: &str, node_addr: &str, node_name_ref: &str) {
+
+
+    let mut client = Client::connect([etcd_url], None).await.unwrap();
+    let node_name = node_name_ref.to_string();
+
+    loop {
+
+        let lease_res = client.lease_grant(10, None).await.unwrap();
+        let lease_id = lease_res.id();
+
+      
+        let (mut keeper, _) = client.lease_keep_alive(lease_id).await.unwrap();
+        tokio::spawn(async move {
+            loop {
+                let _ = keeper.keep_alive().await;
+                tokio::time::sleep(Duration::from_secs(5)).await;
+            }
+        });
+
+        println!("{} (Standby): Waiting for lock...", node_name);
+
+        
+        let mut lock_client = client.lock_client();
+
+        if let Ok(_) = lock_client.lock("oms_global_lock", Some(LockOptions::new().with_lease(lease_id))).await {
+            
+            println!(">>> {} WON ELECTION!", node_name);
+            
+            client.put("engine/leader", node_addr, None).await.unwrap();
+
+            let listener = TcpListener::bind(node_addr).await.unwrap();
+            while let Ok((mut socket, _)) = listener.accept().await {
+                let mut buf = [0u8; 1024];
+                if let Ok(n) = socket.read(&mut buf).await {
+                    let msg = String::from_utf8_lossy(&buf[..n]);
+                    println!("{} log data: {}.", node_name, msg.trim());
+                    
+                    let mut file = OpenOptions::new().append(true).create(true).open("central_order_book.log").await.unwrap();
+                    file.write_all(msg.as_bytes()).await.unwrap();
+                }
+            }
+        }
+
+
     }
 }
